398e926dad2992c8e9922862d3519440a7b8fcf6
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-03-28 17:42:28 +0200

    commit per tutor

--- Vite_DC_Comics/src/components/ThisMainJumbo.vue
+++ Vite_DC_Comics/src/components/ThisMainJumbo.vue
@@ -18,26 +18,45 @@ export default {
 
         </div>
         <div class="cardlist">
-            <img :src="img">
-            <h2> {{ text }} ciao</h2>
+            <div class="card">
+                <img :src="img">
+                <h2> {{ text }} </h2>
+            </div>
+            <button>LOAD MORE</button>
         </div>
     </div>
 </template>
 
 <style scoped>
-.containermain {}
-
 .jumboTron {
     width: 100%;
     height: 370px;
     background-image: url(../public/jumbotron.jpg);
 }
 
+button {
+    border: none;
+    width: 150px;
+    background-color: rgb(5, 130, 249);
+    color: white;
+    padding: 10px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
 .cardlist {
     width: 100%;
     background-color: rgb(27, 27, 27);
     display: flex;
+    justify-content: center;
     flex-wrap: wrap;
+}
+
+.card {
+    width: calc(100% / 6);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
 h2 {
